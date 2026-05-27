fcdd7874fdc8e0540640fd8fe808a77eb1d8d900
fujidaiti <fujidaiti@gmail.com>
2026-05-28 00:50:09 +0900

    [server] Add api to get today's paper

--- server/api/server.go
+++ server/api/server.go
@@ -2,25 +2,48 @@ package api
 
 import (
 	"context"
+	"database/sql"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net"
 	"net/http"
+	"os"
 	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func StartServer(ctx context.Context) {
+	dsn := os.Getenv("DB_DSN")
+	if len(dsn) == 0 {
+		panic("DB_DSN is requried.")
+	}
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		fmt.Println("Closing DB...")
+		if err := db.Close(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+	if err := db.Ping(); err != nil {
+		panic(err)
+	}
+
+	h := &handler{db}
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /health", getHealth)
+	mux.HandleFunc("GET /health", h.getHealth)
+	mux.HandleFunc("GET /papers/today", h.getTodaysPaper)
 
 	srv := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 		// Slowloris attack prevention
 		ReadHeaderTimeout: 5 * time.Second,
-		BaseContext: func(_ net.Listener) context.Context {
-			return ctx
-		},
+		BaseContext:       func(_ net.Listener) context.Context { return ctx },
 	}
 	defer func() {
 		fmt.Println("Shutting down API server...")
@@ -48,6 +71,77 @@ func StartServer(ctx context.Context) {
 	}
 }
 
-func getHealth(w http.ResponseWriter, _ *http.Request) {
+type handler struct {
+	db *sql.DB
+}
+
+func (h *handler) getHealth(w http.ResponseWriter, _ *http.Request) {
 	w.Write([]byte("Feeling good!"))
+}
+
+type getTodaysPaperResponse struct {
+	ID          int        `json:"id"`
+	Issue       int        `json:"issue"`
+	PublishedAt time.Time  `json:"published_at"`
+	Articles    []articles `json:"articles"`
+}
+
+type articles struct {
+	ID          int        `json:"id"`
+	Title       string     `json:"title"`
+	Description *string    `json:"description,omitempty"`
+	PublishedAt *time.Time `json:"published_at,omitempty"`
+}
+
+func (h *handler) getTodaysPaper(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	res := getTodaysPaperResponse{}
+	err := h.db.QueryRowContext(ctx, `
+		SELECT id, issue, published_at
+		FROM papers
+		ORDER BY published_at DESC
+		LIMIT 1;
+	`).Scan(&res.ID, &res.Issue, &res.PublishedAt)
+	if err != nil {
+		serverError(w)
+		return
+	}
+
+	rows, err := h.db.QueryContext(ctx, `
+		SELECT id, title, description, published_at
+		FROM articles
+		WHERE paper_id = $1
+		ORDER BY published_at DESC;
+	`, res.ID)
+	if err != nil {
+		serverError(w)
+		return
+	}
+	for rows.Next() {
+		a := articles{}
+		err := rows.Scan(&a.ID, &a.Title, &a.Description, &a.PublishedAt)
+		if err != nil {
+			serverError(w)
+			return
+		}
+		res.Articles = append(res.Articles, a)
+	}
+	if err := rows.Err(); err != nil {
+		serverError(w)
+		return
+	}
+
+	jres, err := json.Marshal(res)
+	if err != nil {
+		serverError(w)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jres)
+}
+
+func serverError(w http.ResponseWriter) {
+	http.Error(w, "Something went wrong...", http.StatusInternalServerError)
 }
