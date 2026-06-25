8cfe6a87d043baef74240caddf5d2d3251a5c15a
fujidaiti <fujidaiti@gmail.com>
2026-06-25 12:00:22 +0900

    [server] Add dev command to manually run feed polling

---
+++ server/cmd/dev/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/fujidaiti/paperdoll/feature/feed"
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "Specify a sub-command.")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "poll":
+		poll()
+
+	default:
+		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", os.Args[1])
+		os.Exit(1)
+	}
+}
+
+// poll fetches feeds and content immediately.
+func poll() {
+	ctx := context.Background()
+	db, err := setUpDB()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	jobs, err := feed.CollectJobs(ctx, db)
+	if err != nil {
+		panic(err)
+	}
+	if len(jobs) == 0 {
+		fmt.Println("No feed found to poll. Skipping.")
+		return
+	}
+	for _, job := range jobs {
+		err := job.Do(ctx)
+		if err != nil {
+			fmt.Print(err)
+		}
+	}
+}
+
+func setUpDB() (*sql.DB, error) {
+	dsn := os.Getenv("DB_DSN")
+	if len(dsn) == 0 {
+		return nil, fmt.Errorf("DB_DSN is requried")
+	}
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		if err2 := db.Close(); err2 != nil {
+			fmt.Println(err2)
+		}
+		return nil, err
+	}
+	return db, nil
+}
