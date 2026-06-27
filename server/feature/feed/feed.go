5bed72036a2f09eb85a556dde11845906eab5158
fujidaiti <fujidaiti@gmail.com>
2026-06-27 21:25:07 +0900

    [server] Extract feed fetching logic from Subscribe function

--- server/feature/feed/feed.go
+++ server/feature/feed/feed.go
@@ -11,7 +11,11 @@ import (
 )
 
 type Feed struct {
-	ID          int
+	ID int
+	FeedAttrs
+}
+
+type FeedAttrs struct {
 	URL         url.URL
 	SiteURL     *url.URL
 	IconURL     *url.URL
@@ -23,55 +27,70 @@ type Feed struct {
 // Feeds are identified by URL and this operation is idempotent;
 // subscribing to the same feed (URL) twice has no additional effect.
 func Subscribe(ctx context.Context, db *sql.DB, fu url.URL) (Feed, error) {
+	// TODO: Check if the f already exists first
 	// TODO: Validate and cleanup the url (check schema, remove tracking params, etc.)
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, fu.String(), nil)
-	if err != nil {
+	var f Feed
+	if a, err := fetchFeed(ctx, fu); err != nil {
+		fmt.Println(err)
 		return Feed{}, err
+	} else {
+		f = Feed{FeedAttrs: a}
 	}
-	// TODO: Use a custom client to mitigate SSRF attacks (+ timeout)
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return Feed{}, err
-	}
-	defer res.Body.Close()
-	if res.StatusCode != http.StatusOK {
-		return Feed{}, fmt.Errorf("Failed to fetch feed: %s", fu.String())
-	}
-	// TODO: Limit body size
-	raw, err := gofeed.NewParser().Parse(res.Body)
-	if err != nil {
-		return Feed{}, err
-	}
-
-	// TODO: Check if the feed already exists first
-	feed := Feed{URL: fu, Title: raw.Title}
 	var su, iu, desc sql.NullString
-	if raw.Link != "" {
-		u, err := url.Parse(raw.Link)
-		if err == nil {
-			feed.SiteURL = u
-			su = sql.NullString{String: raw.Link, Valid: true}
-		}
+	if u := f.IconURL; u != nil {
+		su = sql.NullString{String: u.String(), Valid: true}
 	}
-	if raw.Image != nil && raw.Image.URL != "" {
-		u, err := url.Parse(raw.Image.URL)
-		if err == nil {
-			feed.IconURL = u
-			iu = sql.NullString{String: raw.Image.URL, Valid: true}
-		}
+	if u := f.IconURL; u != nil {
+		su = sql.NullString{String: u.String(), Valid: true}
 	}
-	if raw.Description != "" {
-		feed.Description = &raw.Description
-		desc = sql.NullString{String: raw.Description, Valid: true}
+	if d := f.Description; d != nil {
+		desc = sql.NullString{String: *d, Valid: true}
 	}
-	err = db.QueryRowContext(ctx, `
+	err := db.QueryRowContext(ctx, `
 		INSERT INTO feeds (url, site_url, icon_url, title, description)
 		VALUES ($1, $2, $3, $4, $5)
 		ON CONFLICT (url) DO UPDATE SET url = EXCLUDED.url
 		RETURNING id;
-	`, feed.URL.String(), su, iu, feed.Title, desc).Scan(&feed.ID)
+	`, f.URL.String(), su, iu, f.Title, desc).Scan(&f.ID)
 	if err != nil {
 		return Feed{}, err
 	}
-	return feed, nil
+	return f, nil
+}
+
+func fetchFeed(ctx context.Context, fu url.URL) (FeedAttrs, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, fu.String(), nil)
+	if err != nil {
+		return FeedAttrs{}, err
+	}
+	// TODO: Use a custom client to mitigate SSRF attacks (+ timeout)
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return FeedAttrs{}, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return FeedAttrs{}, fmt.Errorf("Failed to fetch feed: %s", fu.String())
+	}
+	// TODO: Limit body size
+	raw, err := gofeed.NewParser().Parse(res.Body)
+	if err != nil {
+		return FeedAttrs{}, err
+	}
+
+	a := FeedAttrs{URL: fu, Title: raw.Title}
+	if raw.Link != "" {
+		if u, err := url.Parse(raw.Link); err == nil {
+			a.SiteURL = u
+		}
+	}
+	if raw.Image != nil && raw.Image.URL != "" {
+		if u, err := url.Parse(raw.Image.URL); err == nil {
+			a.IconURL = u
+		}
+	}
+	if raw.Description != "" {
+		a.Description = &raw.Description
+	}
+	return a, nil
 }
