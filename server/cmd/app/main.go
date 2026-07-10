d264249c6653438aad14228a1d73751765bac645
fujidaiti <fujidaiti@gmail.com>
2026-07-10 16:22:17 +0900

    wip

--- server/cmd/app/main.go
+++ server/cmd/app/main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"embed"
 	"fmt"
 	"os"
 	"os/signal"
 
 	"github.com/fujidaiti/paperdoll/api"
 	"github.com/fujidaiti/paperdoll/worker"
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
+
+//go:embed db/migration/*.sql
+var embedMigrations embed.FS
 
 func main() {
 	if len(os.Args) < 2 {
@@ -22,6 +28,9 @@ func main() {
 
 	case "schedule":
 		schedule()
+
+	case "migrate":
+		migrate()
 
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", os.Args[1])
@@ -39,4 +48,30 @@ func schedule() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
 	worker.StartScheduler(ctx)
+}
+
+func migrate() {
+	db, err := setUpDB()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+}
+
+func setUpDB() (*sql.DB, error) {
+	dsn := os.Getenv("DB_DSN")
+	if len(dsn) == 0 {
+		return nil, fmt.Errorf("DB_DSN is requried")
+	}
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		if err2 := db.Close(); err2 != nil {
+			fmt.Println(err2)
+		}
+		return nil, err
+	}
+	return db, nil
 }
