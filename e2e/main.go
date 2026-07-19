4fb434cd50e4e5e66887ef8ea1351127438d8517
fujidaiti <fujidaiti@gmail.com>
2026-07-19 17:07:37 +0900

    add sample socket connection impl

--- e2e/main.go
+++ e2e/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"encoding/json"
 	"fmt"
+	"net"
 	"os"
 	"os/exec"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
 func main() {
@@ -19,6 +23,24 @@ func main() {
 }
 
 func run(ctx context.Context) error {
+	sock, err := net.Listen("tcp", "127.0.0.1:9000")
+	if err != nil {
+		return fmt.Errorf("faild to open a socket: %w", err)
+	}
+	defer func() { _ = sock.Close() }()
+	go func() {
+		for {
+			conn, err := sock.Accept()
+			if err != nil {
+				return
+			}
+			reqCtx, cancel := context.WithTimeout(ctx, 60*time.Second)
+			// Expects exactly one request per session.
+			handleRequest(reqCtx, conn)
+			cancel()
+		}
+	}()
+
 	testCmd := exec.CommandContext(ctx,
 		"patrol",
 		"test",
@@ -48,5 +70,44 @@ func run(ctx context.Context) error {
 	case <-ctx.Done():
 		// Wait for the test command to finish
 		return <-testDone
+	}
+}
+
+type request struct {
+	DebugLabel string `json:"debug_label"`
+	ScenarioID string `json:"scenario_id"`
+}
+
+const _delimitor = "\n"
+
+func handleRequest(ctx context.Context, conn net.Conn) {
+	defer func() { _ = conn.Close() }()
+	if t, ok := ctx.Deadline(); ok {
+		if err := conn.SetDeadline(t); err != nil {
+			fmt.Fprintf(os.Stderr, "failed to set deadline: %v\n", err)
+			return
+		}
+	}
+
+	scanner := bufio.NewScanner(conn)
+	if !scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintf(os.Stderr, "failed to read request: %v\n", err)
+		}
+		return
+	}
+
+	var req request
+	if err := json.Unmarshal(scanner.Bytes(), &req); err != nil {
+		fmt.Fprintf(os.Stderr, "malformed request: %v\n", err)
+		return
+	}
+
+	if _, err := fmt.Fprintf(os.Stdout, "received: %+v\n", req); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to log request: %v\n", err)
+	}
+	time.Sleep(5 * time.Second)
+	if _, err := conn.Write([]byte("ready" + _delimitor)); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to send a response: %v\n", err)
 	}
 }
--- e2e/test/sample_test.dart
+++ e2e/test/sample_test.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:e2e/helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:patrol/patrol.dart';
@@ -8,6 +9,10 @@ void main() {
   patrolTest(
     'counter state is the same after going to home and switching apps',
     ($) async {
+      await setUpServer(
+        debugLabel: 'sample test',
+        scenarioId: 'sample_test_scenario',
+      );
       // Replace later with your app's main widget
       await $.pumpWidgetAndSettle(
         MaterialApp(
