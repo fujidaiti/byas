98a449ac6bb9056be911abb84d3d88f26cc72d3d
fujidaiti <fujidaiti@gmail.com>
2026-07-29 13:04:38 +0900

    refactor

--- client/e2e/helper.go
+++ client/e2e/helper.go
@@ -1,6 +1,27 @@
 package main
 
-import "time"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+type seeder = func(ctx context.Context, db *sql.DB) error
+
+var seeders = map[string]seeder{
+	"newspaper_today":    seedNewspaperSuit_Today,
+	"auth_no_users":      seedAuthSuit_NoUsers,
+	"auth_existing_user": seedAuthSuit_ExistingUser,
+}
+
+func seedDB(ctx context.Context, db *sql.DB, seederID string) error {
+	s, ok := seeders[seederID]
+	if !ok {
+		return fmt.Errorf("no seeder is registered for ID=%q", seederID)
+	}
+	return s(ctx, db)
+}
 
 // mustTimeUTC parses s into a [time.Time]. The accepted format is "yyyy-MM-dd hh:mm:ss".
 func mustTimeUTC(s string) time.Time {
